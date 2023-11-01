4d6e997c0fd279e15ee47872e086c6d46e4ddf93
Engschamm <149345263+Engschamm@users.noreply.github.com>
2023-11-01 07:25:32 +0700

    Add files via upload

---
+++ Conditionswitch/main.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	//kiem tra so nguyen duong hay am
+//	int n;
+//	printf("nhap n: ");
+//	scanf("%d", &n);
+//	switch (n >= 0) {
+//		case 1: 
+//			printf("so %d la so nguyen duong", n);
+//			break; 
+//		case 0: 
+//			printf("so %d la so nguyen am", n);
+//			break; 
+//	} 
+	//n chia het cho 3 hay 5
+//	int n;
+//	printf("nhap n: ");
+//	scanf("%d", &n); 
+//	switch( n % 15){
+//		case 0: 
+//			printf("n chia het cho 3 va 5", n); 
+//			break;
+//		default:
+//			printf("n khong chia het cho 3 va 5", n); 
+//			break; 
+//	} 
+	//nhap vao thang trong nam. in ra so ngay cua thang do
+//	 int thang;
+//	 printf("nhap thang: ");
+//	 scanf("%d", &thang);
+//	 switch (thang){
+//	 	case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+//		 printf("thang co 31 ngay");
+//		 break;
+//		 case 4: case 6: case 9: case 11:
+//		 printf("thang co 30 ngay");
+//		 break;
+//		 default:
+//		 printf("thang khong hop le"); 
+//	 } 
+	//giai phuong trinh bac 2
+//	int a, b, c, delta, x1, x2, x;
+//	printf("phuong trinh bac hai ax^2 + bx + c = 0 \n");
+//	printf("nhap a b c: ")
+//	scanf("%d, %d, %d", &a, &b, &c);
+//	delta = b * b - 4 * a * c;
+//	switch (delta > 0){
+//		case 1: 
+//		x1 = ( -b + sqrt(delta)) / (2 * a);
+//		x2 = ( -b - sqrt(delta)) / (2 * a);
+//		printf("phuong trinh co hai nghiem: x1 = %.2d, x2 = %.2d\n", x1, x2); 
+//		break;
+//		case 0: 
+//		x = -b / (2 * a);
+//		printf("phuong trinh co mot nghiem kep: x = %.2d\n", x);
+//		break; 
+//		
+//	} 
+	return 0;
+}
